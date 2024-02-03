4f0743ffeea53a3dfe34e0e8883a332bc787a472
Hakan <ahmethakant961@gmail.com>
2024-02-03 20:58:46 +0300

    v2

---
+++ src/hav.java
@@ -0,0 +1,15 @@
+public class hav {
+    public static void main(String[] args) {
+
+        int x =6;
+        int y =9;
+        System.out.println(x+y);
+        System.out.println("Versiyon 1 tamamlandı.");
+
+        // versiyon 2
+        System.out.println(x*y);
+        System.out.println("Versiyon 2 tamamlandı.");
+
+
+    }
+}
